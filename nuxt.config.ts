7ce076d214b1e3152f2abf61a0411344994e253e
ooker <maaker@gmail.com>
2023-03-14 08:34:16 +0200

    no-tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css',
-    '~/assets/css/test.css'  
-  ],
+    css: [
+      '~/assets/css/test.css'  
+    ],
     postcss: {
       plugins: {
         tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
 
     <h1>Tere - index</h1>
 
-    <iframe src="testpeedu.pdf" frameborder="0" height="500"></iframe>
+    <!-- <iframe src="testpeedu.pdf" frameborder="0" height="500"></iframe> -->
 
 </template>
