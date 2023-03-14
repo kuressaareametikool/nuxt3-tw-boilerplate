32dc96277d63271e54743b2875600a3a53c55163
ooker <maaker@gmail.com>
2023-03-14 09:57:15 +0200

    dummy data

--- pages/teine.vue
+++ pages/teine.vue
@@ -1,8 +1,18 @@
 <template>
     <!-- custom css that comes from style block at the bottom of this document -->
-    <h1 class="custom-css" v-for="i in 8">
-        Tekst {{ i }}
-    </h1>
+    
+    <section class="grid grid-cols-3">
+   
+        <div v-for="item in mingidAndmed" :key="item.title"
+        class="p-4 bg-white shadow rounded">
+            <h1 class="custom-css" >
+                {{ item.title }}
+            </h1>
+            <p>
+                {{ item.descr }}
+            </p>
+        </div>
+    </section>
 
     
 
@@ -13,10 +23,28 @@
     definePageMeta({
         layout: "other-layout",
     });
+
+    const mingidAndmed= ref(
+        [
+            {
+                "title": "Pealkiri 1",
+                "descr": "Siin on mingi sisu"
+            },
+            {
+                "title": "Pealkiri 2",
+                "descr": "Siin on mingi sisu 2"
+            },
+            {
+                "title": "Pealkiri 3",
+                "descr": "Siin on mingi sisu 3"
+            },
+        ]
+    
+    );
 </script>
 
 <style scoped>
     .custom-css {
-        font-size:5rem;
+        font-size:2rem;
     }
 </style>
